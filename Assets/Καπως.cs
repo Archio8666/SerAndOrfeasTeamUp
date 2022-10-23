88627a10678fd70537a60dc9a067a856e4e9ff35
Orfeas <orfeassavvaidis@gmail.com>
2022-10-23 13:40:10 +0300

    FunctionAdded

---
+++ Assets/Καπως.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Καπως : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void DisplayName()
+    {
+        Debug.Log("My name is Ser Nikolas");
+    }
+}
